39d2ee53ae2fbaaba7eb68f5065226aea5e77cd6
Gemy <gemy46349@gmail.com>
2024-02-26 23:23:39 +0200

    some changes in profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,47 +1,101 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faGear, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faUser } from "@fortawesome/free-regular-svg-icons";
+import {
+  faGear,
+  faPen,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import "./profile.css";
 export default function Profile() {
   return (
     <>
       <div className="profile">
         <div className="slider">
-          <h1>user profile</h1>
+          <h1>profile</h1>
           <ul>
-            <li>
-              <FontAwesomeIcon className="usericon" icon={faUser} />{" "}
-              <button className="user-info"> user info</button>
+            <li className="active">
+              <FontAwesomeIcon className="usericon" icon={faUser} />
+              <span className="user-info"> User info</span>
             </li>
             <li>
-              <FontAwesomeIcon className="settingicon" icon={faGear} />{" "}
-              <button className="setting">setting</button>
+              <FontAwesomeIcon className="settingicon" icon={faGear} />
+              <span className="setting">Setting</span>
+            </li>
+            <li>
+              <FontAwesomeIcon className="settingicon" icon={faBell} />
+              <span className="setting">Notifications</span>
             </li>
           </ul>
-
-          <button className="log-out">
-            {" "}
-            log out{" "}
+          <button className="log-out mt-auto mb-3">
             <FontAwesomeIcon
               className="logout-icon"
               icon={faRightFromBracket}
             />
+            <span>Log out</span>
           </button>
         </div>
-        <div className="profile-info">
+        <div className="profile-info my-3">
           <div className="info">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj0LFhRvv9VG_gWlAS2HYkGe4Enn2V-BvHBg&usqp=CAU"></img>
-            <h3>luka adel</h3>
+            <div className="image">
+              <img
+                alt="upload file"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj0LFhRvv9VG_gWlAS2HYkGe4Enn2V-BvHBg&usqp=CAU"
+              />
+              <label htmlFor="imageInput">
+                <FontAwesomeIcon icon={faPen} />
+              </label>
+              <input
+                type="file"
+                id="imageInput"
+                accept=".png, .jpg, .jpeg"
+                hidden
+              />
+            </div>
+            <div className="text">
+              <h3 className="m-0">luka adel</h3>
+              <p className="m-0">Cairo, Egypt</p>
+            </div>
           </div>
-          <div className="proifle-form">
-            <form>
-              <input type="text" placeholder="first name"></input>
-              <input type="text" placeholder="last name"></input>
+          <div className="proifle-form px-4">
+            <form action="#" className="row g-4">
+              <div className="col-sm-6">
+                <label className="form-label m-0 text-muted">First Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First Name"
+                />
+              </div>
+              <div className="col-sm-6">
+                <label className="form-label m-0 text-muted">Last Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Last Name"
+                />
+              </div>
+              <div className="col-sm-6">
+                <label className="form-label m-0 text-muted">Phone</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Phone Number"
+                />
+              </div>
+              <div className="col-sm-6">
+                <label className="form-label m-0 text-muted">Location</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Location"
+                />
+              </div>
+              {/* <input type="text" placeholder="last name" />
 
-              <input type="text" placeholder="phone"></input>
-              <input type="email" placeholder="email"></input>
+              <input type="text" placeholder="phone" />
+              <input type="email" placeholder="email" /> */}
 
-              <input className="submit" type="submit"></input>
+              <button className="submit rounded-3 mt-4">Save Changes</button>
             </form>
           </div>
         </div>
